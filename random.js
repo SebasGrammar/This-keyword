dcca4ef8cb31ce3e65c1bb213c49b4fe07731922
Sebastian <sebasgrammar@hotmail.com>
2020-02-15 21:11:43 -0500

    added aditional examples and random experiments. going to add comments soon

---
+++ random.js
@@ -0,0 +1,84 @@
+function test() {
+  //test.nickname = "Super function"
+  console.log(this.nickname)
+  return () => {
+    console.log(this.nickname)
+    return function() {
+      console.log(this)
+    }
+  }
+}
+
+//console.log(test.name)
+test.nickname = "Super test"
+//console.log(test.nickname) // Super test
+test.call(test)()() // Super test
+// After all, functions are objects, too...
+test()()() // undefined, undefined, object.window, as expected
+
+/*
+
+function test(callback) {
+  callback(this)
+}
+
+test.nickname = "Little test"
+
+function log() {
+  console.log(this.nickname, typeof this, this.hasOwnProperty("nickname"))
+
+}
+
+test(log.bind(test))
+
+*/
+
+Object.prototype.log = function() {
+  console.log(this)
+}
+
+function pack() {
+  return "good"
+}
+
+"sebas".log()
+pack.log()
+
+/*
+
+function foo() {
+  this.a = 2;
+  this.bar();
+}
+
+function bar() {
+  console.log("No")
+  console.log(this.a);
+}
+
+let test = foo.bind(foo)
+foo.bar = bar.bind(foo)
+test()
+foo.bar.call(foo)
+
+*/
+
+console.log(test.hasOwnProperty("nickname"))
+
+/*******************************************/
+
+let test = {
+  log() {
+    function loggy() {
+      console.log(this)
+    }
+    const arrow = () => console.log(this)
+    loggy()
+    arrow()
+  }
+}
+
+test.log.bind(test)()
+
+
+
